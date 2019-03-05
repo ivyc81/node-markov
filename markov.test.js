21ae5134580918dcd0cca16e879c499b009ad56b
Chengsi <gaochengsi@gmail.com>
2019-03-05 15:37:07 -0800

    implement test for makeText

--- markov.test.js
+++ markov.test.js
@@ -3,15 +3,40 @@ const {MarkovMachine} = require('./markov');
 describe("createMarkovMachine", function(){
     let testText = "The most happy monkey eats banana all day"
     let testInstance;
-
+    let testText2 = "The most happy banana monkey eats banana all day"
+    let testInstance2;
     beforeEach(function(){
         testInstance = new MarkovMachine(testText);
+        testInstance2 = new MarkovMachine(testText2)
     })
 
-    test("", function(){
+    test("the chainOfWords object created correctly", function(){
+        let chainOfWords = testInstance.makeChains();
+        let chainOfWords2 = testInstance2.makeChains();
+        expect(Object.keys(chainOfWords).length).toEqual(8);
+        expect(Object.keys(chainOfWords2).length).toEqual(8);
+        expect(chainOfWords2['banana']).toContain('monkey','all');
+        expect(chainOfWords2['day']).toEqual([null]);
+    })
+
+    test("the makeText function works", function(){
         let chainOfWords = testInstance.makeChains();
 
-        expect(chainOfWords.length).toEqual(testText.length);
+        let sentence = testInstance.makeText();
+        let sentence2 = testInstance.makeText(5);
+        
+        let senArr = sentence.split(' ');
+        let senArr2 = sentence2.split(' ');
 
+        expect(senArr.every(w => chainOfWords.hasOwnProperty(w))).toBe(true);
+        if (senArr.length !== 1){
+            expect(chainOfWords[senArr[0]].indexOf(senArr[1]) !== -1).toBe(true);
+        } 
+        else {
+            expect(chainOfWords[senArr[0]]).toContain(null);
+        }
+        expect(senArr2.length <= 5).toBe(true);
     })
+
+    
 })
